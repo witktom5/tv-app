1e5b44b033acebc8f74c490b56db1da44a842c09
witktom5 <witktom5@gmail.com>
2022-05-02 13:23:35 +0200

    basic setup, install tailwind daisyui

---
+++ src/utils/theme.js
@@ -0,0 +1,28 @@
+function setTheme(themeName) {
+  localStorage.setItem('theme', themeName);
+  document.documentElement.setAttribute('data-theme', themeName);
+}
+
+function saveTheme() {
+  if (localStorage.getItem('theme')) {
+    if (localStorage.getItem('theme') === 'dark') {
+      setTheme('night');
+    } else if (localStorage.getItem('theme') === 'light') {
+      setTheme('light');
+    }
+  } else {
+    if (
+      window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches
+    ) {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  }
+}
+
+module.exports = {
+  setTheme,
+  saveTheme,
+};
