02e42ea0765fcedc0ca9fee9b8b8398b105f0382
witktom5 <witktom5@gmail.com>
2022-05-03 12:46:32 +0200

    setup basic search logic on the api

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,8 @@
+function Spinner() {
+  return (
+    <div className='loading-spinner-container'>
+      <div className='loading-spinner'></div>
+    </div>
+  );
+}
+export default Spinner;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,36 +1,52 @@
 import { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import Header from '../components/Header';
+import axios from 'axios';
+import Spinner from '../components/Spinner';
 
 function Home() {
   const [searchText, setSearchText] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const API_URL = 'https://api.tvmaze.com/search/shows?q=';
 
-  const onClick = () => {
-    console.log('clicked');
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setIsLoading(true);
+      const res = await axios.get(`${API_URL}${searchText}`);
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
   };
 
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <section className='w-4/5'>
       <Header title={'Search TV Shows'} />
       <div>
-        <div className='form-control'>
-          <div className='input-group justify-center mt-7'>
-            <input
-              type='text'
-              placeholder='Search...'
-              className='input input-bordered w-full max-w-lg'
-              value={searchText}
-              onChange={(e) => setSearchText(e.target.value)}
-            />
-            <button
-              className='btn btn-square'
-              aria-label='search'
-              onClick={onClick}
-            >
-              <FaSearch className='w-5 h-5' />
-            </button>
+        <form onSubmit={onSubmit}>
+          <div className='form-control'>
+            <div className='input-group justify-center mt-7'>
+              <input
+                type='text'
+                placeholder='Search...'
+                className='input input-bordered w-full max-w-lg'
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
+              />
+              <button
+                type='submit'
+                className='btn btn-square'
+                aria-label='search'
+              >
+                <FaSearch className='w-5 h-5' />
+              </button>
+            </div>
           </div>
-        </div>
+        </form>
       </div>
     </section>
   );
