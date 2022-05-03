e5269f0e974c943583326b11c8d493d0ba8573a9
witktom5 <witktom5@gmail.com>
2022-05-03 16:02:35 +0200

    style search items

--- src/components/SearchItem.jsx
+++ src/components/SearchItem.jsx
@@ -1,22 +1,66 @@
 import noPhoto from '../assets/nophoto.svg';
 
-function SearchItem({ name, type, genres, image, summary }) {
+function SearchItem({
+  name,
+  language,
+  genres,
+  image,
+  summary,
+  runtime,
+  status,
+  premiered,
+  ended,
+}) {
+  // shorten summary to max 300 characters
+
+  const shortenSummary = (summary) => {
+    if (summary) {
+      if (summary.toString().length < 300) return summary;
+      return summary.toString().slice(3, 300).trim() + '... (more in details)';
+    } else {
+      return 'Summary unavaiable';
+    }
+  };
+
   return (
-    <div className='bg-base-300 rounded-xl shadow-xl mb-8 flex'>
-      <div className='flex flex-col'>
+    <div className='bg-base-200 rounded-xl shadow-xl mb-8 flex flex-col sm:flex-row gap-2'>
+      <div className='flex flex-row sm:flex-col bg-base-300 flex-none rounded-t-xl lg:rounded-l-xl'>
         <img
           src={image ?? noPhoto}
           alt={name}
-          className='tv-show-img object-cover bg-no-repeat rounded-xl rounded-tr-none rounded-bl-none'
+          className='tv-show-img object-scale-down bg-no-repeat rounded-tl-xl'
         />
-        <div>Hey hey</div>
-      </div>
-      <div className='flex flex-col'>
-        <h2 className='card-title'>{name}</h2>
-        <div dangerouslySetInnerHTML={{ __html: summary }} />
-        <div className='card-actions justify-end'>
-          <button className='btn btn-primary'>Watch</button>
+        <div className='p-3 flex flex-col justify-center h-full'>
+          <p>
+            <strong>Language:</strong> {language ?? 'Unknown'}
+          </p>
+          <p>
+            <strong>Runtime:</strong> {runtime ? runtime + ' min' : 'Unknown'}
+          </p>
+          <p>
+            <strong>Premiere:</strong> {premiered ?? 'Unknown'}
+          </p>
+          <p>
+            <strong>Status:</strong> {status ? status : 'Unknown'}
+          </p>
+          {status === 'Ended' && (
+            <p>
+              <strong>Ended:</strong> {ended ?? 'Unknown'}
+            </p>
+          )}
         </div>
+      </div>
+      <div className='flex flex-col gap-4 p-3 pr-6 w-full'>
+        <h2 className='card-title text-2xl pt-2 pb-4'>{name}</h2>
+        <div
+          dangerouslySetInnerHTML={{ __html: shortenSummary(summary) }}
+          className='my-auto'
+        />
+        <button className='btn btn-block mt-auto'>Details</button>
+        <p>
+          <strong>Genres:</strong>{' '}
+          {genres && genres.length > 0 ? genres.join(', ') : 'Unknown'}
+        </p>
       </div>
     </div>
   );
--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -22,11 +22,11 @@ function SearchResults() {
   };
 
   return (
-    <section className='w-4/5 lg:w-3/5'>
+    <section className='w-4/5 sm:w-4/5 lg:w-1/2 xl:w-1/3 2xl:w-1/3'>
       <Header title={`Search results for "${searchText}"`} />
       <button
         onClick={onClickBack}
-        className='btn btn-outline btn-sm flex gap-2 mb-4'
+        className='btn btn-outline btn-sm flex gap-2 mb-6'
       >
         <FaArrowLeft /> Back to Search
       </button>
@@ -38,6 +38,12 @@ function SearchResults() {
               key={id}
               name={el.show.name}
               summary={el.show.summary}
+              language={el.show.language}
+              runtime={el.show.runtime}
+              genres={el.show.genres}
+              status={el.show.status}
+              premiered={el.show.premiered}
+              ended={el.show.ended}
               image={
                 el.show.image
                   ? el.show.image.medium ?? el.show.image.original
